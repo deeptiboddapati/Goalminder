39f50579cfadb0b96fc452abd7d4c1ade97cac0b
deeptiboddapati <deeptiboddapati@gmail.com>
2016-07-08 14:23:16 -0500

    tracking busy time as a set to prevent over counting overlapping events

--- createdayslots.js
+++ createdayslots.js
@@ -56,9 +56,22 @@ events.forEach(function(item,index){
 
 
 days.forEach(function(item){
-	item.events.eventduration =0
+	item.busytotal =0
+	item.busytimes = new Set();
+	item.freetotal = 144
 	for(var i = 0; i <item.events.length;i++){
 		//console.log(item.events[i])
-		item.events.eventduration +=item.events[i].duration
+		//item.events.eventduration +=item.events[i].duration
+		// duration = 340 and startingpoint = 120 then 120/10 = 12 and duration/10 = 34
+		//s needs to go from 12>34
+
+		var startingpoint = (item.events[i].start.diff(item.midnight,'minutes'))/10
+		for(var s = startingpoint; s < startingpoint+item.events[i].duration/10; s ++  ){
+			item.busytimes.add(s)
+
+		}
+
 	}
+	item.busytotal = item.busytimes.size
+	item.freetotal -= item.busytotal
 })
